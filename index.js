15b8e4c2b89cd4d8b6bb140ea4eb73d5edbcf50a
monwar23 <monwar2345@gmail.com>
2024-05-13 03:44:24 +0600

    added delete operation

--- index.js
+++ index.js
@@ -102,6 +102,13 @@ async function run() {
       res.send(result)
     })
 
+    app.delete("/foods/:id",async(req,res)=>{
+      const id=req.params.id;
+      const query={_id: new ObjectId(id)}
+      const result=await foodsCollection.deleteOne(query)
+      res.send(result)
+    })
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
